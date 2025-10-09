b05ab8919376885768474eae04601c920915af2a
Sergiy Sanin <sanin_sergiy@yahoo.com>
2025-10-09 01:20:52 +0000

    sandboxing

---
+++ src/bin/feed1.rs
@@ -0,0 +1,28 @@
+use tungstenite::{client::IntoClientRequest, stream::MaybeTlsStream, WebSocket, Message};
+use tungstenite::client_tls;
+use url::Url;
+use std::net::TcpStream;
+use rustls::crypto::aws_lc_rs;
+use rustls::crypto::CryptoProvider;
+
+fn main() {
+    let _ = CryptoProvider::install_default(aws_lc_rs::default_provider());
+
+    let url = Url::parse("wss://stream.binance.com:9443/ws/ethbtc@depth20").unwrap();
+    let req = url.as_str().into_client_request().unwrap();
+    let domain = url.domain().unwrap();
+
+    let tcp = TcpStream::connect((domain, 9443)).unwrap();
+    let (mut socket, _) = client_tls(req, tcp).unwrap();
+    println!("Connected to Binance depth20 feed");
+    loop {
+        match socket.read_message() {
+            Ok(Message::Text(txt)) => println!("{}", txt),
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
---
+++ src/bin/grpc_sub.rs
@@ -0,0 +1,34 @@
+use prost::Message;
+use std::io::{Read};
+use std::net::TcpStream;
+
+pub mod pubsub {
+    include!(concat!(env!("OUT_DIR"), "/pubsub.rs"));
+}
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:5000")?;
+    println!("Connected to publisher.");
+
+    loop {
+        // Read length prefix
+        let mut len_buf = [0u8; 4];
+        if stream.read_exact(&mut len_buf).is_err() {
+            println!("Publisher closed connection");
+            break;
+        }
+        let msg_len = u32::from_be_bytes(len_buf) as usize;
+
+        // Read message bytes
+        let mut buf = vec![0u8; msg_len];
+        if stream.read_exact(&mut buf).is_err() {
+            println!("Stream ended");
+            break;
+        }
+
+        let msg = pubsub::Message::decode(&*buf).unwrap();
+        println!("Received [{}]: {}", msg.topic, msg.content);
+    }
+
+    Ok(())
+}
